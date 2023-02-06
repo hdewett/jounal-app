959b7bc3297b972a7582a581aa7cd534f45dd737
Ethan <ethan.steip@gmail.com>
2023-02-06 11:28:48 -0500

    continued work on entries dash table

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,11 @@
-
-import React, { useState, useEffect} from 'react';
-import axios from  'axios';
 import EntriesTable from './components/EntriesTable';
 import './App.css';
 
-const entriesUrl = "/api/entries";
-
 function App() {
-
-const [entriesData, setEntriesData] = useState([]);
-
-useEffect(() => {
-  getEntriesDataWithAxios();
-}, []);
-
-const getEntriesDataWithAxios = async () => {
-  const response = await axios.get(entriesUrl);
-  setEntriesData(response.data);
-};
 
   return (
     <div className="App" data-theme="emerald">
       <EntriesTable />
-      <div className=''>
-        {entriesData.map((entry) => {
-          return <p key={entry.id}><span className='text-2xl font-bold'>{entry.id}:</span> {entry.body}</p>
-        })}
-        Hello World!
-      </div>
     </div>
   );
 }
--- client/src/components/EntriesTable.jsx
+++ client/src/components/EntriesTable.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Entry from './Entry';
+import axios from  'axios';
+
+const entriesUrl = "/api/entries";
 
 export default function EntriesTable(props) {
+
+  const [entriesData, setEntriesData] = useState([]);
+
+  useEffect(() => {
+    getEntriesDataWithAxios();
+  }, []);
+  
+  const getEntriesDataWithAxios = async () => {
+    const response = await axios.get(entriesUrl);
+    setEntriesData(response.data);
+  };
+
+  // Create Appointment components array.
+  const entryArray = entriesData.map((entry) => {
+
+    return <Entry
+        key={entry.id}
+        id={entry.id}
+        title={entry.title}
+        date={entry.date}
+        />;
+});
 
 
   return (
@@ -16,7 +41,7 @@ export default function EntriesTable(props) {
         </tr>
       </thead>
       <tbody>
-        <Entry />
+        { entryArray }
       </tbody>
     </table>
   </div>
