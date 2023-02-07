fb91f68769a38b0b463e6961448fe6f53bff9caa
nawalabdi <nawalabdi03@gmail.com>
2023-02-06 19:36:49 -0500

    setup entriesfeed

--- client/src/pages/EntriesFeed.jsx
+++ client/src/pages/EntriesFeed.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router";
+import EntriesTable from "../components/EntriesTable";
 
 function EntriesFeed() {
   return (
     
     <div className="EntriesFeed">
       <h1>NEW entry</h1>
+      <EntriesTable />
       <Outlet />
     </div>
   )
