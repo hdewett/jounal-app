e446c8ba1e16156e9c24414a4ac9e9e14aaa1591
Hilora Dewett <h.dewett@outlook.com>
2023-02-08 19:24:57 -0500

    update styling for sidenav

--- client/src/components/SideNavBar.jsx
+++ client/src/components/SideNavBar.jsx
@@ -23,13 +23,14 @@ function SideNavBar() {
     <div className="drawer-side">
     <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
     <ul className="menu p-4 w-50 bg-base-200  text-base-content">
-    <a className="btn btn-ghost normal-case text-xl text-primary">Codeful</a>
+    <a className="btn btn-ghost normal-case text-2xl text-primary">Codeful</a>
       {/* <!-- Sidebar content here --> */}
-      <li><Link to="/" className={classNames({"active":pathName == "/"})}><AiIcons.AiFillHome /> Dashboard</Link></li>
-      <li><Link to="/entries" className={classNames({"active":pathName == "/entries"})}><BsIcons.BsJournalCode />Journal</Link></li>
-      <li><Link to="/newentry" className={classNames({"active":pathName == "/newentry"})}><AiIcons.AiOutlinePlus />New Entry</Link></li>
-      <li><Link to="/entriesfeed" className={classNames({"active":pathName == "/entriesfeed"})}><FaIcons.FaTasks />Entries Feed</Link></li>
-      <li><Link to="/stats" className={classNames({"active":pathName == "/stats"})}><IoIcons.IoMdStats />Stats</Link></li>
+      
+      <li className="text-lg"><Link to="/" className={classNames({"active":pathName == "/"})}><AiIcons.AiFillHome /> Dashboard</Link></li>
+      <li className="text-lg"><Link to="/entries" className={classNames({"active":pathName == "/entries"})}><BsIcons.BsJournalCode />Journal</Link></li>
+      <li className="text-lg"><Link to="/newentry" className={classNames({"active":pathName == "/newentry"})}><AiIcons.AiOutlinePlus />New Entry</Link></li>
+      <li className="text-lg"><Link to="/entriesfeed" className={classNames({"active":pathName == "/entriesfeed"})}><FaIcons.FaTasks />Entries Feed</Link></li>
+      <li className="text-lg"><Link to="/stats" className={classNames({"active":pathName == "/stats"})}><IoIcons.IoMdStats />Stats</Link></li>
 
       {/* {SideNavData.map((item, index) => {
             return (
