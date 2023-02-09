cfda4a32291a5506a8b2d379ff6a74e8ba618eb2
Hilora Dewett <h.dewett@outlook.com>
2023-02-09 10:25:05 -0500

    update routes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,20 @@
 import { useState } from 'react';
-import EntriesTable from './components/EntriesTable';
 import './App.css';
-import { createRoot } from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
-  Routes,
-  Link,
-  Outlet,
-  ReactDOM,
   createRoutesFromElements,
-  BrowserRouter
 } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
 import EntriesFeed from "./pages/EntriesFeed";
 import Entries from './pages/Entries';
-import NewEntry from './pages/NewEntry';
 import Stats from './pages/Stats';
 import SideNavBar from './components/SideNavBar';
 import CalendarSmall from './components/CalendarSmall'
 import ViewEntry from './pages/ViewEntry';
 import EditEntry from './pages/EditEntry'
-import TinyMCE from './components/TinyMCE';
 import EntryForm2 from './components/EntryForm2';
 
 function App() {
@@ -45,10 +36,10 @@ function App() {
     createRoutesFromElements(
       <Route element={<AppLayout />}>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/Entries" element={<Entries />} />
-        <Route path="/newEntry" element={<EntryForm2 />} />
+        <Route path="/entries" element={<EntriesFeed />} />
+        <Route path="/entry/new" element={<EntryForm2 />} />
         <Route path="/stats" element={<Stats />} />
-        <Route path="/entriesfeed" element={<EntriesFeed />} />
+        <Route path="/entriesfeed" element={<Entries />} />
         <Route path="/calendar" element={<CalendarSmall/>} />
         <Route path="/viewentry/:id" element={<ViewEntry/>} />
         <Route path="/entry/:id" element={<EditEntry/>} />
