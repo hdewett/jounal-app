a82686c1e458903aae1d8b05bb428d508fcd67ec
nawalabdi <nawalabdi03@gmail.com>
2023-02-10 15:46:12 -0500

    test

--- server/routes/entries.js
+++ server/routes/entries.js
@@ -1,8 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-module.exports = db => {
-
+module.exports = (db) => {
   // Get all entries for User 1 (default user), Limit 5
   router.get("/entries/limit", (req, response) => {
     db.query(
@@ -13,11 +12,12 @@ module.exports = db => {
       FROM entries
       WHERE user_id = 1
       LIMIT 5;`
-    ).then(({ rows: entries }) => {
-      console.log("retrieving entries");
-      return response.json(entries);
-    })
-      .catch(error => {
+    )
+      .then(({ rows: entries }) => {
+        console.log("retrieving entries");
+        return response.json(entries);
+      })
+      .catch((error) => {
         console.log(`There was an ${error}`);
         response.status(500).send;
       });
@@ -32,11 +32,12 @@ module.exports = db => {
         to_char(date,'YYYY-MM-DD') AS date
       FROM entries
       WHERE user_id = 1;`
-    ).then(({ rows: entries }) => {
-      console.log("retrieving entries");
-      return response.json(entries);
-    })
-      .catch(error => {
+    )
+      .then(({ rows: entries }) => {
+        console.log("retrieving entries");
+        return response.json(entries);
+      })
+      .catch((error) => {
         console.log(`There was an ${error}`);
         response.status(500).send;
       });
@@ -57,10 +58,11 @@ module.exports = db => {
       INNER JOIN users ON user_id = users.id
       WHERE entries.private = false
       order by date desc;`
-    ).then(({ rows: entries }) => {
-      return response.json(entries);
-    })
-      .catch(error => {
+    )
+      .then(({ rows: entries }) => {
+        return response.json(entries);
+      })
+      .catch((error) => {
         console.log(`There was an ${error}`);
         response.status(500).send;
       });
@@ -83,79 +85,89 @@ module.exports = db => {
       INNER JOIN frameworks on framework_id = frameworks.id
       INNER JOIN languages on language_id = languages.id
       WHERE entries.id = $1;
-      ;`, [req.params.id]
-    ).then(({ rows: entries }) => {
-      console.log("retrieving specific entry", entries);
-      return response.json(entries[0]);
-    })
-      .catch(error => {
+      ;`,
+      [req.params.id]
+    )
+      .then(({ rows: entries }) => {
+        console.log("retrieving specific entry", entries);
+        return response.json(entries[0]);
+      })
+      .catch((error) => {
         console.log(`There was an ${error}`);
         response.status(500).send();
       });
   });
 
-  // Get sum of  entries, Get total languages, Get amount of distinct languages, Get total wordcount:  to count individual entries add a WHERE userid = userid 
-router.get("/stats", (req, response) => {
-  Promise.all([
-    db.query(`SELECT 
-    SUM(hours) 
-    FROM entries`),
-    db.query(`SELECT
-        DISTINCT language_id,
-        languages.name as language_name
-        FROM entries 
-        INNER JOIN languages on language_id = languages.id;`),
-    db.query(`SELECT
-        language_id, count(*) as language_amount,
-        languages.name as language_name
-        FROM entries   
-        INNER JOIN languages on language_id = languages.id
-        Group by language_name, language_id; `),
-    db.query(`SELECT 
-        id, 
-        title, 
-        entry,
-        notes
-        FROM entries;` )
-  ])
-    .then((results) => {
-      console.log(results[0].rows[0].sum, results[1].rows, results[2].rows)
-      response.send({
-        hours: results[0].rows[0].sum,
-        languages: results[1].rows,
-        distinctLanguage: results[2].rows,
-        entries: results[3].rows
+  // Get sum of  entries, Get total languages, Get amount of distinct languages, Get total wordcount:  to count individual entries add a WHERE userid = userid
+  router.get("/stats", (req, response) => {
+    Promise.all([
+      db.query(`SELECT 
+      SUM(hours) 
+      FROM entries),
+      db.query(SELECT
+          DISTINCT language_id,
+          languages.name as language_name
+          FROM entries 
+          INNER JOIN languages on language_id = languages.id;`),
+      db.query(`SELECT
+          language_id, count(*) as language_amount,
+          languages.name as language_name
+          FROM entries   
+          INNER JOIN languages on language_id = languages.id
+          Group by language_name, language_id;` ),
+      db.query(`SELECT 
+          id, 
+          title, 
+          entry,
+          notes
+          FROM entries; `),
+      db.query(`SELECT
+          date, 
+          count(*) 
+          FROM entries 
+          GROUP BY date;` )
+    ])
+      .then((results) => {
+        console.log(results[0].rows[0].sum, results[1].rows, results[2].rows)
+        response.send({
+          hours: results[0].rows[0].sum,
+          languages: results[1].rows,
+          distinctLanguage: results[2].rows,
+          entries: results[3].rows,
+          entriesPerDay: results[4].rows
+        });
+      })
+      .catch(error => {
+        console.log(`There was an ${error}`);
+        response.status(500).send;
       });
-    })
-    .catch(error => {
-      console.log(`There was an ${error}`);
-      response.status(500).send;
-    });
-});
+  });
 
-
-// Get specific entry for stats
-router.get("/stats/:id", (request, response) => {
-  db.query(`SELECT 
+  // Get specific entry for stats
+  router.get("/stats/:id", (request, response) => {
+    db.query(
+      `SELECT 
       id, 
       title, 
       entry,
       notes,
-      FROM entries WHERE id = $1`, [
-    request.params.id
-  ]).then(({ rows: entries }) => {
-    console.log("retrieving entry");
-    return response.json(entries);
-  })
-    .catch(error => {
-      console.log(`There was an ${error}`);
-      response.status(500).send;
-    });
-});
+      FROM entries WHERE id = $1`,
+      [request.params.id]
+    )
+      .then(({ rows: entries }) => {
+        console.log("retrieving entry");
+        return response.json(entries);
+      })
+      .catch((error) => {
+        console.log(`There was an ${error}`);
+        response.status(500).send;
+      });
+  });
 
   // Get specific entry
   router.get("/entries/:id", (request, response) => {
-    db.query(`SELECT 
+    db.query(
+      `SELECT 
         id, 
         title, 
         to_char(date,'YYYY-MM-DD') AS date,
@@ -165,13 +177,14 @@ router.get("/stats/:id", (request, response) => {
         language_id, 
         framework_id, 
         notes
-        FROM entries WHERE id = $1::integer`, [
-      request.params.id
-    ]).then(({ rows: entries }) => {
-      console.log("retrieving entry");
-      return response.json(entries);
-    })
-      .catch(error => {
+        FROM entries WHERE id = $1::integer`,
+      [request.params.id]
+    )
+      .then(({ rows: entries }) => {
+        console.log("retrieving entry");
+        return response.json(entries);
+      })
+      .catch((error) => {
         console.log(`There was an ${error}`);
         response.status(500).send;
       });
@@ -179,23 +192,37 @@ router.get("/stats/:id", (request, response) => {
 
   // Update specific entry
   router.put("/entries/:id", (request, response) => {
-    const { title, date, entry, hours, language, framework, notes, private } = request.body;
-    db.query(`UPDATE entries SET title=$2, date=$3, entry=$4, hours=$5, language_id=$6, framework_id=$7, notes=$8, private=$9  WHERE id = $1::integer`, [
-      request.params.id, title, date, entry, hours, language, framework, notes, private
-    ]).then(({ rows: entries }) => {
-      console.log("updated entry");
-      return response.json(entries);
-    })
-      .catch(error => {
+    const { title, date, entry, hours, language, framework, notes, private } =
+      request.body;
+    db.query(
+      `UPDATE entries SET title=$2, date=$3, entry=$4, hours=$5, language_id=$6, framework_id=$7, notes=$8, private=$9  WHERE id = $1::integer`,
+      [
+        request.params.id,
+        title,
+        date,
+        entry,
+        hours,
+        language,
+        framework,
+        notes,
+        private,
+      ]
+    )
+      .then(({ rows: entries }) => {
+        console.log("updated entry");
+        return response.json(entries);
+      })
+      .catch((error) => {
         console.log(`There was an ${error}`);
         response.status(500).send;
       });
   });
 
   // Update Existing entry
-  router.put('/entries/:id', (req, res) => {
-    const { title, entry, hours, language, framework, notes, date, private } = req.body;
-    
+  router.put("/entries/:id", (req, res) => {
+    const { title, entry, hours, language, framework, notes, date, private } =
+      req.body;
+
     db.query(
       `UPDATE entries
        SET
@@ -207,52 +234,63 @@ router.get("/stats/:id", (request, response) => {
        notes = $6, 
        date = $7
        private = $8
-       WHERE id = $9;` ,[title, entry, hours, language, framework, notes, date, private, req.params.id]
-    ).then(({ rows: entries }) => {
-      console.log(entries);
-      return res.status(200).json(entries);
-  
-    })
-      .catch(error => {
+       WHERE id = $9;`,
+      [
+        title,
+        entry,
+        hours,
+        language,
+        framework,
+        notes,
+        date,
+        private,
+        req.params.id,
+      ]
+    )
+      .then(({ rows: entries }) => {
+        console.log(entries);
+        return res.status(200).json(entries);
+      })
+      .catch((error) => {
         console.log(`There was an ${error}`);
         res.status(500).send;
       });
   });
-
 
   // Create a new entry
-  router.post('/entries', (req, res) => {
-    const { title, entry, hours, language, framework, notes, date, private } = req.body;
-    
+  router.post("/entries", (req, res) => {
+    const { title, entry, hours, language, framework, notes, date, private } =
+      req.body;
+
     console.log("Request Body: ", req.body);
     db.query(
-      `INSERT INTO entries (title, entry, hours, language_id, framework_id, notes, date, user_id, private) VALUES ($1,$2,$3,$4,$5,$6,$7,1,$8) returning *;` ,[title, entry, hours, language, framework, notes, date, private]
-    ).then(({ rows: entries }) => {
-      console.log(entries);
-      return res.status(200).json(entries);
-  
-    })
-      .catch(error => {
+      `INSERT INTO entries (title, entry, hours, language_id, framework_id, notes, date, user_id, private) VALUES ($1,$2,$3,$4,$5,$6,$7,1,$8) returning *;`,
+      [title, entry, hours, language, framework, notes, date, private]
+    )
+      .then(({ rows: entries }) => {
+        console.log(entries);
+        return res.status(200).json(entries);
+      })
+      .catch((error) => {
         console.log(`There was an ${error}`);
         res.status(500).send;
       });
   });
-
 
   // Delete entry
   router.delete("/entries/:id", (request, response) => {
     db.query(`DELETE FROM entries WHERE id = $1::integer RETURNING *;`, [
-      request.params.id
-    ]).then(({ rows: entries }) => {
-      console.log("retrieving entries");
-      return response.json(entries);
-    })
-      .catch(error => {
+      request.params.id,
+    ])
+      .then(({ rows: entries }) => {
+        console.log("retrieving entries");
+        return response.json(entries);
+      })
+      .catch((error) => {
         console.log(`There was an ${error}`);
         response.status(500).send;
       });
   });
 
-  
   return router;
 };
